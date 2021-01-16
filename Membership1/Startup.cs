fba3165bb11e73994e18a40795eb3b2055277779
John Oregbemi <FIC00344@fbninsurance.com>
2021-01-16 22:54:13 +0100

    Add project files.

---
+++ Membership1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Membership1.Startup))]
+namespace Membership1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
